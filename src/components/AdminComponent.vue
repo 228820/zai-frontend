7669fa01574441c18e0085978ebc11f98a5ec400
Piotr Orczykowski <228820@edu.p.lodz.pl>
2023-02-05 14:38:44 +0100

    Add users list

--- src/components/AdminComponent.vue
+++ src/components/AdminComponent.vue
@@ -17,12 +17,21 @@ export default {
         }
     },
     async mounted() {
-        const API_URL = 'http://localhost:8080/api/messages/'
-        const user = JSON.parse(localStorage.getItem('user'))
-        const response = await axios.get(API_URL + 'admin', {
-            headers: { Authorization: 'Bearer ' + user.accessToken },
-        })
-        this.content = response.data.message
+        try {
+            const API_URL = 'http://localhost:8080/api/messages/'
+            const user = JSON.parse(localStorage.getItem('user'))
+            const response = await axios.get(API_URL + 'admin', {
+                headers: { Authorization: 'Bearer ' + user.accessToken },
+            })
+            this.content = response.data.message
+        } catch (e) {
+            if (e.response.status == 403) {
+                this.content =
+                    "You don't have perrmission to access this resource"
+            } else {
+                this.content = 'Something went wrong, please try again later'
+            }
+        }
     },
 }
 </script>
--- src/components/UsersListComponent.vue
+++ src/components/UsersListComponent.vue
@@ -2,12 +2,60 @@
     <div class="container">
         <header class="jumbotron">
             <h1>Here will be users list</h1>
+            <h3 v-if="content.length != 0">{{ content }}</h3>
         </header>
+        <div v-for="user in users" :key="user.id" class="card">
+            <div class="card-header">Id: {{ user.id }}</div>
+            <div class="card-body">
+                <h5 class="card-title">Username: {{ user.username }}</h5>
+                <p class="card-text">Email: {{ user.email }}</p>
+                <p class="card-text">Role: {{ user?.roles?.[0]?.role }}</p>
+
+                <a href="#" class="btn btn-primary">Edit User</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'UserListComponent',
+    data() {
+        return {
+            users: [],
+            content: '',
+        }
+    },
+    async mounted() {
+        try {
+            const API_URL = 'http://localhost:8080/api/users/'
+            const user = JSON.parse(localStorage.getItem('user'))
+            if (user.role == 'ADMIN') {
+                const response = await axios.get(API_URL + `?page=${1}`, {
+                    headers: { Authorization: 'Bearer ' + user.accessToken },
+                })
+                console.log(response)
+                this.users = response.data.content
+            } else if (user.role == 'USER') {
+                const response = await axios.get(API_URL + `${user.id}`, {
+                    headers: { Authorization: 'Bearer ' + user.accessToken },
+                })
+                this.users = [response.data]
+                console.log(response.data)
+            } else {
+                this.content =
+                    "You don't have perrmission to access this resource"
+            }
+        } catch (e) {
+            if (e.response.status == 403) {
+                this.content =
+                    "You don't have perrmission to access this resource"
+            } else {
+                this.content = 'Something went wrong, please try again later'
+            }
+        }
+    },
 }
 </script>
